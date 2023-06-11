46ed3908bd0b57f13cf34ab6dc2beb460363a14a
Askar Oralkhan <askar4ik123@gmail.com>
2023-06-11 18:11:21 +0600

    Added first enemy

--- Assets/Scripts/Enemy/MossGiant.cs
+++ Assets/Scripts/Enemy/MossGiant.cs
@@ -6,21 +6,36 @@ public class MossGiant : Enemy
 {
     private Vector3 _currentTarget;
     private Animator _anim;
+    private SpriteRenderer _spriteRenderer;
     
     // Start is called before the first frame update
     void Start()
     {
         _anim = transform.GetChild(0).GetComponent<Animator>();
+        _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
     } 
 
     // Update is called once per frame
     public override void Update()
     {
+        if (_anim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            return;
+        }
         Movement();
     }
 
     void Movement()
     {
+        if (_currentTarget == pointA.position)
+        {
+            _spriteRenderer.flipX = true;
+        }
+        else if (_currentTarget == pointB.position)
+        {
+            _spriteRenderer.flipX = false;
+        }
+
         if (transform.position == pointA.position)
         {
             _currentTarget = pointB.position;
@@ -32,13 +47,6 @@ public class MossGiant : Enemy
             _anim.SetTrigger("Idle");
         }
 
-        if (_anim.GetCurrentAnimatorStateInfo(0).IsName("Idle")){
-            Debug.Log("Idling");
-            return;
-        }
-        else
-        {
-            transform.position = Vector3.MoveTowards(transform.position, _currentTarget, speed * Time.deltaTime);
-        }
+        transform.position = Vector3.MoveTowards(transform.position, _currentTarget, speed * Time.deltaTime);
     }
 }
